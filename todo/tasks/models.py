d002d45e92197fa4bd726c32ffe0bc8378df7d13
Prakash Thapa <prakashthap617@gmail.com>
2022-01-25 12:02:30 +0545

    first commit

---
+++ todo/tasks/models.py
@@ -0,0 +1,11 @@
+from django.db import models as m
+
+# Create your models here.
+
+class Task(m.Model):
+	title = m.CharField(max_length=200)
+	complete = m.BooleanField(default=False)
+	created = m.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
